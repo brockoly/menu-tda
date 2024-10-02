6fb42f927ea0bbde60a5e5ce1bb0518772367fd3
ameneses <amenesesm@falabella.cl>
2024-10-02 14:28:45 -0300

    <amenesesm> | changing title and logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './assets/images/logo.png';
 import './App.css';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import logo from './assets/images/logo.png';
 import lomo from './assets/images/lomo.jpg';
 import cazuela from './assets/images/cazuela.jpg';
 
@@ -10,18 +11,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Image src={logo} rounded width="200rem"/>
       </header>
       <body className="mainContainer">
-        <p>{todaysMenu}</p>
-        <Card style={{ width: '26rem' }}>
+        <h2>{todaysMenu}</h2>
+        <br/>
+        <Card style={{ width: '24rem' }}>
           <Card.Img variant="top" src={lomo} />
           <Card.Body>
             <Card.Title>Lomo Saltado</Card.Title>
           </Card.Body>
         </Card>
         <br/>
-        <Card style={{ width: '26rem' }}>
+        <Card style={{ width: '24rem' }}>
           <Card.Img variant="top" src={cazuela} />
           <Card.Body>
             <Card.Title>Cazuela de vacuno</Card.Title>
