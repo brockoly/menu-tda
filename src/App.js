2259ec40fce1e8285b249a02d97b63c8d7a2f45a
ameneses <amenesesm@falabella.cl>
2024-10-05 14:37:13 -0300

    <amenesesm> | adds new style and image

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
-import logo from './assets/images/logo.png';
+import jubileo from './assets/images/jubileo.png';
 import pollo from './assets/images/pollo.jpg';
 import fideos from './assets/images/fideos.jpg';
 
 function App() {
   const date = new Date();
-  const todaysMenu = `El menú de hoy ${date.toLocaleString('es-ES', {weekday:'long'})} es:`;
+  const todaysMenu = `Menú ${date.toLocaleString('es-ES', {weekday:'long'})} ${date.toLocaleDateString('es-ES')}:`;
   return (
     <div className="App">
       <header className="App-header">
-        <Image src={logo} rounded width="200rem"/>
+        <Image src={jubileo} className="logo-image" rounded/>
       </header>
       <body className="mainContainer">
         <h2>{todaysMenu}</h2>
@@ -19,7 +19,7 @@ function App() {
         <Card style={{ width: '24rem' }}>
           <Card.Img variant="top" src={pollo} />
           <Card.Body>
-            <Card.Title>Pollo al horno con puré de papas</Card.Title>
+            <Card.Title>Pollo al horno con puré rústico</Card.Title>
           </Card.Body>
         </Card>
         <br/>
@@ -30,6 +30,7 @@ function App() {
           </Card.Body>
         </Card>
       </body>
+      <h6 className='footer'>Las imágenes son solo referenciales</h6>
     </div>
   );
 }
