38670a8d343c48bbf6c95e705cabf9de68e2df89
ameneses <amenesesm@falabella.cl>
2024-10-05 13:24:08 -0300

    <amenesesm> | saturday menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 import logo from './assets/images/logo.png';
-import lomo from './assets/images/lomo.jpg';
-import cazuela from './assets/images/cazuela.jpg';
+import pollo from './assets/images/pollo.jpg';
+import fideos from './assets/images/fideos.jpeg';
 
 function App() {
   const date = new Date();
-  const todaysMenu = `El menú de hoy ${date.toLocaleDateString('es-ES')} es:`;
+  const todaysMenu = `El menú de hoy ${date.toLocaleString('es-ES', {weekday:'long'})} es:`;
   return (
     <div className="App">
       <header className="App-header">
@@ -17,16 +17,16 @@ function App() {
         <h2>{todaysMenu}</h2>
         <br/>
         <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src={lomo} />
+          <Card.Img variant="top" src={pollo} />
           <Card.Body>
-            <Card.Title>Lomo Saltado</Card.Title>
+            <Card.Title>Pollo al horno con puré de papas</Card.Title>
           </Card.Body>
         </Card>
         <br/>
         <Card style={{ width: '24rem' }}>
-          <Card.Img variant="top" src={cazuela} />
+          <Card.Img variant="top" src={fideos} />
           <Card.Body>
-            <Card.Title>Cazuela de vacuno</Card.Title>
+            <Card.Title>Fideos a la boloñesa</Card.Title>
           </Card.Body>
         </Card>
       </body>
